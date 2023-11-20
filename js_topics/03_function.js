05b1668eb783434305f7496b0615ffcf66713c24
pradeepadlakondawar <94275183+pradeepadlakondawar@users.noreply.github.com>
2023-11-20 11:12:30 +0000

    practice

--- js_topics/03_function.js
+++ js_topics/03_function.js
@@ -2,11 +2,22 @@
 
 // the function which accepts the other functions as a parameter or returns a function are callled as higher order function
 
-function fun1(inp) {
-   return function (inp2) {
-        console.log(inp+inp2);
-    }
+// function fun1(inp) {
+//    return function (inp2) {
+//         console.log(inp+inp2);
+//     }
+// }
+// // const funResult = fun1(10);
+// // funResult(20)
+// fun1(10)(30);
+
+
+//call
+//call k help se ham function ko run krte samay function k andr this ki value kya rahegi decide kr sakte hai.
+let obj1 ={username:"pro"}
+
+function call1(inp) {
+    console.log(this);
 }
-// const funResult = fun1(10);
-// funResult(20)
-fun1(10)(30);
+// call1()
+call1.call(obj1)
