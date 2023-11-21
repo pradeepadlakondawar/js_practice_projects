07126a05c16a6c008185949aaa2148f12d72ff21
pradeepadlakondawar <94275183+pradeepadlakondawar@users.noreply.github.com>
2023-11-21 16:42:16 +0000

    practice

---
+++ js_topics/quetionSolving.js
@@ -0,0 +1,40 @@
+// Q.1 
+// Write a js program to write check two numbers and retrun true if one of the number is 100 or sum of the two numbers are hunderd.
+
+// let sum =(a, b)=>{
+//     if(a===100 || b=== 100 || (a+b) ==100 ){
+//         return true
+//     }
+//     else return false;
+// }
+
+// let sum =(a,b)=> a===100 || b=== 100 || (a+b) ==100
+// console.log(sum(10,900))
+
+
+//Q.2
+// write a program to check the file extension name
+
+// let checkExtension=(inp)=>{
+//     let ans =inp.split('.')
+//     return ans[1]
+// }
+
+// const getFileExtension=(str)=>str.slice(str.lastIndexOf("."))
+// console.log(getFileExtension("kjkjkj.html"));
+
+//convert string to Array
+// let str ="helloWorld"
+// let arr=Array.from(str)
+
+//convert array to string
+// let arr = [1,2,3,4,5,6]
+// console.log(arr.join());
+
+// indexOf method
+// let sentence = "Once upon a time there was a King"
+// // console.log(sentence.indexOf("O"));
+// console.log(sentence.lastIndexOf("a"));
+// console.log(sentence.slice(sentence.lastIndexOf("a")));
+
+
